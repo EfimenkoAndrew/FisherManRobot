fb58375641f17519b3480f41c45a8f623b751ab5
Efimenko Andrew <andrew.efimenko.office@gmail.com>
2019-03-17 15:45:03 +0200

    Added simple UI project
    Signed-off-by: Efimenko Andrew <andrew.efimenko.office@gmail.com>

--- TesseractOCRWrapper/dllmain.cpp
+++ TesseractOCRWrapper/dllmain.cpp
@@ -9,14 +9,6 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-	{
-		char tesseractPath[256];
-		if (::GetEnvironmentVariableA("TESSDATA_PREFIX", tesseractPath, 256) <= 0)
-		{
-			throw std::exception("TESSDATA_PREFIX (path to tesseract language) environment variable is not set. Download language https://github.com/tesseract-ocr/tessdata and set environment variable.");
-		}
-	}
-	break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
